f8003fff2a23c84d19e88cc386ffc136147dc17f
Minho_Baek_01 <58423056+hoeo159@users.noreply.github.com>
2026-02-07 22:50:29 +0900

    update GunData

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -32,10 +32,20 @@ public class Gun : MonoBehaviour {
 
     private void Awake() {
         // 사용할 컴포넌트의 참조 가져오기
+        gunAudioPlayer = GetComponent<AudioSource>();
+        bulletLineRenderer = GetComponent<LineRenderer>();
+
+        bulletLineRenderer.positionCount = 2;
+        bulletLineRenderer.enabled = false;
     }
 
     private void OnEnable() {
         // 총 상태 초기화
+        ammoRemain = gunData.startAmmoRemain;
+        magAmmo = gunData.magCapacity;
+
+        state = State.Ready;
+        lastFireTime = 0f;
     }
 
     // 발사 시도
